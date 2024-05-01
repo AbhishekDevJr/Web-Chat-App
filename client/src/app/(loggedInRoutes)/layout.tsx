82b3c6e532608f82b05c9d3315c873a537403cf3
abhishek <abhishek007coc@gmail.com>
2024-05-01 18:19:59 +0530

    Dashboard UI development in progress, Complete change in design of dashboard component in being implemented

--- client/src/app/(loggedInRoutes)/layout.tsx
+++ client/src/app/(loggedInRoutes)/layout.tsx
@@ -1,7 +1,8 @@
 'use client';
 import Link from "next/link";
 import { useState } from "react";
-import { addFriendSvg, friendListSvg, friendReqSvg, logOutSvg } from "@/helpers/constants";
+import { addFriendSvg, fakeRequestData, friendListSvg, friendReqSvg, logOutSvg, notificationSvg, searchSvg, sendSvg } from "@/helpers/constants";
+import { Input } from "antd";
 
 export default function LoggedInLayout({
     children
@@ -10,8 +11,8 @@ export default function LoggedInLayout({
 }) {
     const [chatSelection, setChatSelection] = useState('chat');
     return (
-        <div className='container-user-layout flex flex-1 min-h-[100vh] border-solid '>
-            <div className='container-sidebar flex flex-col justify-between border-[E4E4E7] border-x-[2px] grow-0 shrink-0 basis-[15%]'>
+        <div className='container-user-layout flex flex-col min-h-[100vh] border-solid bg-[white]'>
+            {/* <div className='container-sidebar flex flex-col justify-between border-[E4E4E7] border-x-[2px] grow-0 shrink-0 basis-[15%]'>
                 <div className='chat-info flex flex-col justify-between items-center gap-[10px] border-solid border-[#E4E4E7] border-b-[2px] p-[20px]'>
                     <Link href='/' className="text-[28px] text-[#6366F1] font-[600] tracking-tighter whitespace-nowrap">Exclusive <span className='text-[#09090B]'>Messenger</span></Link>
                     <Link href='/signup' className='text-[#F5F5F5] font-[600] px-[35px] py-[8px] rounded-[8px] bg-[#18181B]'>AI CHAT</Link>
@@ -35,17 +36,17 @@ export default function LoggedInLayout({
 
                 <div className='friend-info p-[20px] border-solid border-[E4E4E7] border-b-[2px]'>
                     <ul className="flex flex-col gap-[20px]">
-                        <Link href='/addfriend' className="text-[#090D22] hover:underline decoration-solid decoration-[#000000]">
+                        <Link href='/addfriend' className="text-[#090D22] p-[10px] rounded-lg hover:bg-[#6366F1] hover:text-[#F5F5F5] focus:bg-[#6366F1] focus:text-[#F5F5F5] duration-200">
                             <li className="flex items-center gap-[5px]">{addFriendSvg}Add Friend</li>
                         </Link>
 
-                        <Link href='/friendlist' className="text-[#090D22] hover:underline decoration-solid decoration-[#000000]">
+                        <Link href='/friendlist' className="text-[#090D22] p-[10px] rounded-lg hover:bg-[#6366F1] hover:text-[#F5F5F5] focus:bg-[#6366F1] focus:text-[#F5F5F5] duration-200">
                             <li className="flex items-center gap-[5px]">{friendListSvg}Friend List</li>
                         </Link>
-                        <Link href='/friendrequests' className="text-[#090D22] hover:underline decoration-solid decoration-[#000000]">
+                        <Link href='/friendrequests' className="text-[#090D22] p-[10px] rounded-lg hover:bg-[#6366F1] hover:text-[#F5F5F5] focus:bg-[#6366F1] focus:text-[#F5F5F5] duration-200">
                             <li className="flex items-center gap-[5px]">{friendReqSvg}Friend Requests</li>
                         </Link>
-                        <Link href='/' className="text-[#090D22] hover:underline decoration-solid decoration-[#000000]">
+                        <Link href='/' className="text-[#090D22] p-[10px] rounded-lg hover:bg-[#6366F1] hover:text-[#F5F5F5] focus:bg-[#6366F1] focus:text-[#F5F5F5] duration-200">
                             <li className="flex items-center gap-[5px]">{logOutSvg}Log Out</li>
                         </Link>
                     </ul>
@@ -58,10 +59,49 @@ export default function LoggedInLayout({
                         <span className="text-[#090D22]">{`abhishek@gmail.com`}</span>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
-            <div className='container-main-section flex-1'>
-                {children}
+            <header className='flex items-center justify-between px-[20px] py-[10px]'>
+                <div className='site-logo'>
+                    <Link href='/' className="text-[28px] text-[#6366F1] font-[600] tracking-tighter whitespace-nowrap">Exclusive <span className='text-[#09090B]'>Messenger</span></Link>
+                </div>
+
+                <div className='site-search flex items-center justify-center gap-[10px]'>
+                    <Input className='py-[10px] px-[20px] min-w-[300px] rounded-[100px] bg-[#F5F7F9] focus:bg-[#F5F7F9]' type='text' placeholder="Search" />
+                    <span className="text-[#F5F5F5] font-[500] px-[20px] py-[8px] rounded-[8px] bg-[#6366F1] cursor-pointer">
+                        {searchSvg}
+                    </span>
+                </div>
+
+                <div className='site-user-info flex items-center justify-center gap-[10px]'>
+                    {notificationSvg}
+                    <span className='min-w-[40px] min-h-[40px] bg-[#6366F1] rounded-[100px] flex items-center justify-center text-[#F5F7F9]'>A</span>
+                </div>
+            </header>
+
+            <div className='container-childred flex justify-between grow-[1] p-[15px]'>
+                <div className='container-sidebar basis-[15%] outline outline-[cyan] outline-[1px]'>
+                    <div className="friend-add">
+                        <ul className='flex justify-between gap-[5px]'>
+                            {fakeRequestData.map((item, index) => <li className="min-w-[40px] min-h-[40px] bg-[#6366F1] rounded-[100px] flex items-center justify-center text-[#F5F7F9]" key={index}>{item.firstName[0]}</li>)}
+
+                            <li className="min-w-[40px] min-h-[40px] bg-[#6366F1] rounded-[100px] flex items-center justify-center text-[#F5F7F9]">+</li>
+                        </ul>
+                    </div>
+
+                    <div className="friend-list">
+                        <ul>
+                            {fakeRequestData.map((item, index) =>
+                                <li key={index}>
+
+                                </li>)}
+                        </ul>
+                    </div>
+                </div>
+
+                <div className='container-main-section flex-1'>
+                    {children}
+                </div>
             </div>
         </div>
     );
--- client/src/components/FriendList/FriendListComp.tsx
+++ client/src/components/FriendList/FriendListComp.tsx
@@ -13,15 +13,15 @@ function FriendListComp() {
 
     return (
         <div className='container-friendlist p-[50px]'>
-            <h1 className='text-[38px] text-[#09090B] font-[600] text-center'>Friend List</h1>
-            <div className='flex items-center justify-center p-[10px]'>
-                <div className='outline outline-[#E4E4E7] outline-[2px] rounded-xl shadow-lg p-[40px] flex flex-col gap-[20px]'>
+            <h1 className='text-[38px] text-[#09090B] font-[600]'>Friend List</h1>
+            <div className='flex items-center p-[10px] outline outline-[#E4E4E7] outline-[2px] rounded-xl'>
+                <div className='rounded-xl p-[40px] flex flex-col gap-[20px]'>
                     <p>Found <span className="font-[600]">{friendList.length} People</span></p>
                     {friendList.map((item: any, index: any) =>
                         <>
                             <div key={index} className='flex items-center justify-between gap-[10px]'>
                                 <span className="bg-[#E4E4E7] min-h-[40px] min-w-[40px] flex items-center justify-center rounded-[100px]">{item.firstName[0]}</span>
-                                <p className={`text-[#C35BB6] font-[600]`}>{`${item.firstName} ${item.lastName}`}</p>
+                                <p className={`text-[#6366F1] font-[600]`}>{`${item.firstName} ${item.lastName}`}</p>
                                 <p className="text-[#6C6468]">{`${item.email}`}</p>
                                 <div className='user-found-actions flex gap-[10px]'>
                                     <button className='text-[#F5F5F5] font-[600] px-[15px] py-[10px] rounded-[5px] bg-[#18181B] flex justify-center items-center gap-[10px]'>Chat{chatSvg}</button>
