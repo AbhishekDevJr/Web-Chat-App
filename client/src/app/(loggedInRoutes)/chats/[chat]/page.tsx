faf207d001dce3b0942228d5fde1963699523c19
Abhishek Choudhari <abhishek007coc@gmail.com>
2025-10-14 22:45:26 +0530

    Feat: Implemented Bi-directional chat feature between users, Also implemented Custom event trigger for Typing between users connected through native websocket

--- client/src/app/(loggedInRoutes)/chats/[chat]/page.tsx
+++ client/src/app/(loggedInRoutes)/chats/[chat]/page.tsx
@@ -41,27 +41,27 @@ export default function ChatUser({ params }: { params: any }) {
         setUserMessage('');
     };
 
-    // const handleTyping = () => {
-    //     if (!isTyping) {
-    //         setIsTyping(true);
-    //         socket.emit('typing', { roomId: params?.chat, currUserData });
-    //     }
+    const handleTyping = () => {
+        const message_payload = {
+            message_type: 'user_type_start',
+            message: 'Temp',
+            sender: currUserData
+        }
+        if (!isTyping) {
+            socket.send(JSON.stringify(message_payload));
+        }
 
-    //     clearTimeout(typingTimeout);
-
-    //     typingTimeout = setTimeout(() => {
-    //         setIsTyping(false);
-    //         socket.emit('stopTyping', { roomId: params?.chat, currUserData });
-    //     }, 2000);
-
-    //     // clearTimeout(typingTimeout);
-    // }
+        typingTimeout = setTimeout(() => {
+            message_payload.message_type = `user_type_stop`
+            socket.send(JSON.stringify(message_payload));
+        }, 2000);
+    }
 
     let typingTimeout: any;
 
     const handleMsgInputChange = (val: any) => {
         setUserMessage(val);
-        // handleTyping();
+        handleTyping();
     }
 
     const getCurrentUserInfo = () => {
@@ -101,14 +101,20 @@ export default function ChatUser({ params }: { params: any }) {
         }
 
         ws.onmessage = (event) => {
-            console.log('Message Received--------->', JSON.parse(event.data));
             const data = JSON.parse(event.data);
+            setSenderUserObj(data?.receiver);
 
             if (data?.Success) {
                 console.log('Client Logs: WebSocket Message Connection Established');
             }
-            else {
-                setMessages((prevMessages: any) => [...prevMessages, data?.message]);
+            else if(data?.message_type === `user_message`) {
+                setMessages((prevMessages: any) => [...prevMessages, data]);
+            }
+            else if(data?.message_type === `user_type_start`){
+                setIsTyping(true);
+            }
+            else if(data?.message_type === `user_type_stop`){
+                setIsTyping(false);
             }
         }
 
@@ -122,7 +128,12 @@ export default function ChatUser({ params }: { params: any }) {
 
     const sendMessage = (msg: String) => {
         if (socket && socket.readyState === WebSocket.OPEN) {
-            socket.send(JSON.stringify({ message: msg, sender: currUserData }));
+            const messagePayload = {
+                message_type : `user_message`,
+                message:msg,
+                sender: currUserData
+            }
+            socket.send(JSON.stringify(messagePayload));
         }
     }
 
@@ -149,23 +160,23 @@ export default function ChatUser({ params }: { params: any }) {
                         {messages.length ?
                             <ul className="min-w-[100%]">
                                 {!isEmpty(messages) && messages.map((item: any, index: any) =>
-                                    <li key={index} className={`flex items-center gap-[5px] ${item?.userMessageData?.userInfo?.username === messages[index + 1]?.userMessageData?.userInfo?.username ? 'mb-[30px]' : ''} ${item?.userMessageData?.userInfo?.username !== currUserData?.username ? 'justify-start' : 'justify-end'} ${index !== 0 ? 'mt-[-20px]' : ''}`}>
-                                        {item?.userMessageData?.userInfo?.username !== currUserData?.username ?
+                                    <li key={index} className={`flex items-center gap-[5px] ${item?.sender?.userid === messages[index + 1]?.sender?.userid ? 'mb-[30px]' : ''} ${item?.sender?.userid !== currUserData?.userid ? 'justify-start' : 'justify-end'} ${index !== 0 ? 'mt-[-20px]' : ''}`}>
+                                        {item?.sender?.userid !== currUserData?.userid ?
                                             <>
-                                                <span className="mt-[-30px] min-w-[40px] min-h-[40px] max-w-[40px] rounded-[100px] flex items-center justify-center text-[#F5F7F9] bg-[#09090B] cursor-pointer">{item?.userMessageData?.userInfo?.first_name[0].toUpperCase()}</span>
+                                                <span className="mt-[-30px] min-w-[40px] min-h-[40px] max-w-[40px] rounded-[100px] flex items-center justify-center text-[#F5F7F9] bg-[#09090B] cursor-pointer">{item?.sender?.first_name[0].toUpperCase()}</span>
                                                 <div className="text-[#09090B] font-[600]">
-                                                    <span className="text-[#09090B] text-[14px]">{item?.userMessageData?.userInfo?.first_name}</span>
-                                                    <p className="bg-[#e2e2e2] p-[15px] rounded-tr-[20px] rounded-br-[20px] rounded-bl-[20px] max-w-[400px]">{item?.userMessageData?.message}</p>
+                                                    <span className="text-[#09090B] text-[14px]">{item?.sender?.first_name}</span>
+                                                    <p className="bg-[#e2e2e2] p-[15px] rounded-tr-[20px] rounded-br-[20px] rounded-bl-[20px] max-w-[400px]">{item?.message}</p>
                                                 </div>
                                             </>
                                             :
                                             <>
                                                 <div className="text-[white] font-[600] flex flex-col justify-end">
-                                                    <span className="text-[#09090B] text-[14px] ml-[auto]">{item?.userMessageData?.userInfo?.first_name}</span>
-                                                    <p className="bg-[#6366F1] p-[15px] rounded-br-[20px] rounded-bl-[20px] rounded-tl-[20px] max-w-[400px]">{item?.userMessageData?.message}</p>
+                                                    <span className="text-[#09090B] text-[14px] ml-[auto]">{item?.sender?.first_name}</span>
+                                                    <p className="bg-[#6366F1] p-[15px] rounded-br-[20px] rounded-bl-[20px] rounded-tl-[20px] max-w-[400px]">{item?.message}</p>
                                                 </div>
 
-                                                <span className="mt-[-30px] min-w-[40px] min-h-[40px] max-w-[40px] rounded-[100px] flex items-center justify-center text-[#F5F7F9] bg-[#153448] cursor-pointer">{item?.userMessageData?.userInfo?.first_name[0].toUpperCase()}</span>
+                                                <span className="mt-[-30px] min-w-[40px] min-h-[40px] max-w-[40px] rounded-[100px] flex items-center justify-center text-[#F5F7F9] bg-[#153448] cursor-pointer">{item?.sender?.first_name[0].toUpperCase()}</span>
                                             </>
                                         }
                                     </li>
@@ -180,7 +191,7 @@ export default function ChatUser({ params }: { params: any }) {
                             </>
                         }
 
-                        {typingMessage && senderUserObj?._id !== currUserData?._id && <p className="absolute bottom-[100px]">{`${typingMessage}`}</p>}
+                        {isTyping && senderUserObj?.userid !== currUserData?.userid && <p className="absolute bottom-[100px]">{`${`${senderUserObj?.first_name} is typing...`}`}</p>}
 
                         <div className="flex items-center min-w-[50%] fixed bottom-[20px]">
                             <Input value={userMessage} className='py-[10px] px-[20px] min-w-[300px] rounded-[100px] bg-[#F5F7F9] hover:bg-[#F5F7F9] hover:border-[#6366F1] focus:border-[#6366F1] focus:bg-[#F5F7F9] text-[16px] hover:border-[2px] border-[2px]' type='text' placeholder="Say Something..." onChange={(e: any) => handleMsgInputChange(e.target.value)} onPressEnter={sendUserMessage} />
